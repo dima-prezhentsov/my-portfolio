6db04552f09b5060db9a591e4c6a6d4b8e3443ff
dmitriy_prezhentsov <dimonpre@gmail.com>
2024-10-01 21:52:01 +0300

    setup gh-pages

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact-section">
+      <h1>Контактная информация</h1>
+      <p>Email: dimonpre@gmail.com</p>
+      <p>Телефон: +7 (999) 999-99-99</p>
+      <p>GitHub: <a href="https://github.com/dima-prezhentsov">dima-prezhentsov</a></p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div className="projects-section">
+      <h1>Мои проекты</h1>
+      <ul>
+        <li>
+          <a href="project-link-1">Проект 1</a>
+        </li>
+        <li>
+          <a href="project-link-2">Проект 2</a>
+        </li>
+        <li>
+          <a href="project-link-3">Проект 3</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
