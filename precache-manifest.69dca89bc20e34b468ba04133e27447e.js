d0853aba2dae2a6630faa0f80622dfede4a486d9
dmitriy_prezhentsov <dimonpre@gmail.com>
2024-10-02 20:38:26 +0300

    Updates

---
+++ precache-manifest.69dca89bc20e34b468ba04133e27447e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e8abec82645d32bd54eba92016fc6b",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d7484b2fa1dccfbf1177",
+    "url": "/my-portfolio/static/css/main.174761eb.chunk.css"
+  },
+  {
+    "revision": "aeb1b20104ae7016388d",
+    "url": "/my-portfolio/static/js/2.a67b60b4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/my-portfolio/static/js/2.a67b60b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad4c0f9a4119b86ecf3",
+    "url": "/my-portfolio/static/js/3.6ce5ce29.chunk.js"
+  },
+  {
+    "revision": "d7484b2fa1dccfbf1177",
+    "url": "/my-portfolio/static/js/main.81ce49a4.chunk.js"
+  },
+  {
+    "revision": "b7e502fb8d61c18504a6",
+    "url": "/my-portfolio/static/js/runtime-main.3b33f927.js"
+  }
+]);
